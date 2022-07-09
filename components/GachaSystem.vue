3940c56f5a94cec6abbe2f83bb627294725e2c26
Danendz <danilapayne@gmail.com>
2022-07-09 06:57:33 +0100

    added loadbar

--- components/GachaSystem.vue
+++ components/GachaSystem.vue
@@ -3,7 +3,10 @@
     <h1 class="notInLandscape">
       Пожалуйста перевени свой телефон чтобы все заработало!
     </h1>
-    <GachaMain />
+    <progress v-show="!isLoaded" :value="progress" max="100">
+      {{ progress }}
+    </progress>
+    <GachaMain v-show="isLoaded" />
   </div>
 </template>
 
@@ -11,6 +14,37 @@
 /* import axios from 'axios' */
 export default {
   name: 'GachaSystem',
+  data() {
+    return {
+      downloadedVideos: [],
+      isLoaded: false,
+      progress: 0,
+    }
+  },
+  videos: [
+    '/gachaVideos/backgroundCropped.mp4',
+    '/gachaVideos/3star1comp.mp4',
+    '/gachaVideos/4star1comp.mp4',
+    '/gachaVideos/4star10comp.mp4',
+    '/gachaVideos/5star1comp.mp4',
+    '/gachaVideos/5star10comp.mp4',
+  ],
+  async beforeMount() {
+    this.isLoaded = false
+    await this.get()
+    this.isLoaded = true
+  },
+  methods: {
+    async get() {
+      await this.$options.videos.forEach(async (link) => {
+        const res = await fetch(link)
+        const blob = await res.blob()
+        this.progress += 16.6
+        console.log(this.progress)
+        console.log(URL.createObjectURL(blob))
+      })
+    },
+  },
 }
 </script>
 
