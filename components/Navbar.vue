eb164011ca3869b2d3a88574b9f56afde04ed943
Danendz <danilapayne@gmail.com>
2022-07-10 10:52:37 +0100

    changed paths of navbar icons

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,16 +2,28 @@
   <div :class="['navbar', { show: active }]">
     <div class="icons">
       <NuxtLink to="/events">
-        <img alt="event" src="/NavbarIcons/icon_Eventse.webp" />
+        <img
+          alt="event"
+          src="https://static.wikia.nocookie.net/gensin-impact/images/d/d4/Icon_Events.png/"
+        />
       </NuxtLink>
       <NuxtLink to="/gacha">
-        <img alt="wish" src="/NavbarIcons/icon_Wishe.webp" />
+        <img
+          alt="wish"
+          src="https://static.wikia.nocookie.net/gensin-impact/images/5/59/Icon_Wish.png/"
+        />
       </NuxtLink>
       <NuxtLink to="/inventory">
-        <img alt="inventory" src="/NavbarIcons/icon_Inventorye.webp" />
+        <img
+          alt="inventory"
+          src="https://static.wikia.nocookie.net/gensin-impact/images/a/a0/Icon_Inventory.png/"
+        />
       </NuxtLink>
       <NuxtLink to="/characters">
-        <img alt="character" src="~assets/NavbarIcons/icon_Character.webp" />
+        <img
+          alt="character"
+          src="https://static.wikia.nocookie.net/gensin-impact/images/1/11/Icon_Character_Aether.png/"
+        />
       </NuxtLink>
     </div>
     <i
