253982d38752e8d40972dcfb79f2035107ba9823
Danendz <danilapayne@gmail.com>
2022-07-10 10:33:12 +0100

    try to fix icons on navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -11,8 +11,7 @@
         <img alt="inventory" src="/NavbarIcons/icon_Inventorye.webp" />
       </NuxtLink>
       <NuxtLink to="/characters">
-        
-        <img alt="character" src="/GachaIcons/icon_Character.webp" />
+        <img alt="character" src="../static/NavbarIcons/icon_Character.webp" />
       </NuxtLink>
     </div>
     <i
